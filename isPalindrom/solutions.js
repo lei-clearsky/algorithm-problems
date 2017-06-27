8ae3ce63839ff923b7e3bd9ad9e1c304123ab60c
David Cameron <flyingace@users.noreply.github.com>
2017-06-26 23:25:52 -0400

    Update solutions.js

--- isPalindrom/solutions.js
+++ isPalindrom/solutions.js
@@ -32,3 +32,21 @@ function isPalindromRecursion(str) {
 
 	return false;
 }
+
+/*
+A simpler solution that I have seen proposed is to 
+1. remove all non-word characters from the string (such as spaces and punctuation) and lower-case all characters
+2. make a copy of the revised string
+3. convert it to an array w/split('')
+4. reverse it
+5. convert it back into a string w/join()
+6. then compare it to the original
+
+I had forgotten before seeing this that a palindrome is not just a word or phrase which is mirrored down the middle,
+but also a word or phrase that when reversed has all the same characters in the same order!
+*/
+
+function isPalindromeViaReversal(str) {
+    str = str.replace(/\W/g, '').toLowerCase();
+    return (str == str.split('').reverse().join(''));
+}
