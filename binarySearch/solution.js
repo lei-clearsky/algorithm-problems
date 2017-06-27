d17925174b5c53488c3a0f208c13ca5d98f18ef5
Norman Chou <normchou@users.noreply.github.com>
2017-06-27 11:14:40 -0700

    Update solution.js

--- binarySearch/solution.js
+++ binarySearch/solution.js
@@ -23,3 +23,29 @@ function binarySearch(arr, val) {
 
 	return -1;
 }
+
+
+// Same implementation, but used different variable names which helped me better understand the function logic
+//
+function binaryIndexOf(arr, searchElement) {
+  
+  var minIndex = 0;
+  var maxIndex = arr.length - 1;
+  var currentIndex;
+  var currentElement;
+   
+  while (minIndex <= maxIndex) {
+    currentIndex = Math.floor((minIndex + maxIndex) / 2);
+    currentElement = arr[currentIndex];
+    
+    if (currentElement > searchElement) {
+      maxIndex = currentIndex - 1 
+    } else if (currentElement < searchElement) {
+      minIndex = currentIndex + 1
+    } else {
+      return currentIndex
+    }
+  }
+  
+  return -1
+}
