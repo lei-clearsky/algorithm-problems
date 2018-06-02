3130f54f9e579a38d272baf8ab2cd9f02aa62871
lei-clearsky <sportzhulei@gmail.com>
2018-06-02 18:12:16 -0400

    Add slides modue

--- simple-slide-show/app.js
+++ simple-slide-show/app.js
@@ -1,11 +1,43 @@
-(function() {
+var mySlides = (function() {
+  var slidesContainer = document.getElementById('slides');
   var slides = document.querySelectorAll('#slides .slide');
   var currentSlide = 0;
-  var slideInterval = setInterval(nextSlide, 2000);
+  var slideInterval;
+
+  function init(w) {
+    var slidesWidth = slides.length * w + 50;
+    slidesContainer.style.width = slidesWidth + 'px';
+  }
 
   function nextSlide() {
-    slides[currentSlide].className = "slide";
     currentSlide = (currentSlide + 1) % slides.length;
-    slides[currentSlide].className = "slide slide__visible";
+    moveSlide();
+  }
+
+  function prevSlide() {
+    currentSlide = (currentSlide - 1) < 0 ? slides.length - 1 : currentSlide - 1;
+    moveSlide();
+  }
+
+  function moveSlide() {
+    currentSlidePosition = currentSlide * 600;
+    slidesContainer.style.left = '-' + currentSlidePosition + 'px';
+    slidesContainer.className = "slides";
+  }
+
+  function autoSlides() {
+    slideInterval = setInterval(nextSlide, 2000);
+  }
+
+  function stopAutoSlides() {
+    clearInterval(slideInterval);
+  }
+
+  return {
+    nextSlide: nextSlide,
+    prevSlide: prevSlide,
+    autoSlides: autoSlides,
+    stopAutoSlides: stopAutoSlides,
+    init: init
   }
 })();
