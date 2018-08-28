a5663528f058277027dfe50bf2840c99cd419035
lei-clearsky <sportzhulei@gmail.com>
2018-08-28 10:21:49 -0400

    Revert int without too much javascript native

--- Algorithm/Easy/reverseInteger/solution.js
+++ Algorithm/Easy/reverseInteger/solution.js
@@ -12,6 +12,23 @@ function isLessThan32bit(num) {
 	return num < 2147483647 ? true : false;
 }
 
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function(x) {  
+    var totalDigits = Math.abs(x).toString().length;
+    var output = 0;
+    while(totalDigits > 0) {
+      var temp = x % 10;
+      output += temp * Math.pow(10, totalDigits - 1);
+      x = x > 0 ? Math.floor(x / 10) : Math.ceil(x / 10);
+      totalDigits--;
+    }
+    if (Math.abs(output) > Math.pow(2,31) - 1) return 0;
+    return output;
+};
+
 reverse(123) // 321
 reverse(-123) // -321
 reverse(12345667899) // 0
