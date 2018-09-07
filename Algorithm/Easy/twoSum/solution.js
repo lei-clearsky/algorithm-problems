d2152011ebcb924368c672aa1253730f6ee4a485
lei-clearsky <sportzhulei@gmail.com>
2018-09-07 09:59:19 -0400

    Add twoSum solution

--- Algorithm/Easy/twoSum/solution.js
+++ Algorithm/Easy/twoSum/solution.js
@@ -18,5 +18,30 @@ var twoSum = function(nums, target) {
     return []
 };
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    var start = 0;
+    var len = nums.length;
+    var sum = [];
+    
+    while (start < len - 1) {
+        var end = start + 1;
+
+        while (end < len) {
+          if (nums[start] + nums[end] === target) {
+              sum.push(start, end);
+              return sum;
+          }
+          end++;
+        }
+        start++;
+    }
+    return sum;
+};
+
 // Tests
 twoSum([3, 2, 4], 6)	// [1, 2]
